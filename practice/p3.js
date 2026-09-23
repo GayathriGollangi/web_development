d386e559acbe50973ea98023160997c80d1525d0
GayathriGollangi <gayathrigollangii@gmail.com>
2026-09-23 15:17:45 +0530

    Write a JavaScript program to check whether a user can log in using a stored username and password.

---
+++ practice/p3.js
@@ -0,0 +1,14 @@
+let stored_username = "john"
+let stored_password ="john"
+let input_username ="john"
+let input_password = "john"
+
+if(stored_username === input_username){
+    if(stored_password === input_password){
+            console.log("user can log in");
+
+    }
+    else {
+    console.log("wrong details")
+}
+}
