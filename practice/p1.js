411814f92101fd860759cf96de491035b1d9eb1a
GayathriGollangi <gayathrigollangii@gmail.com>
2026-09-23 12:34:08 +0530

    Write a JavaScript program to check whether a given number is divisible by 3 or not.

---
+++ practice/p1.js
@@ -0,0 +1,14 @@
+// Write a JavaScript program to check whether a given number is divisible by 3 or not.
+let num =5;
+
+if(num%3 === 0){
+
+    console.log("Divisible by 3");
+
+}
+
+else{
+
+    console.log("not divisible by 3")
+
+}
